0c920536ce6e8019c8429175bdd9b0781b40e31c
Spicer Matthews <spicer@cloudmanic.com>
2025-07-06 12:03:53 -0700

    Fix module path

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/example/evernote-cli
+module github.com/cloudmanic/evernote-cli
 
 go 1.24.3
 
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/example/evernote-cli/cmd"
+import "github.com/cloudmanic/evernote-cli/cmd"
 
 func main() {
 	if err := cmd.Execute(); err != nil {
